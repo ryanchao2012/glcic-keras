105bb51c6a1509e4702baeb4a783662e698773ed
ryanchao2012 <ryanchao2012@gmail.com>
2018-07-08 17:21:53 +0800

    update evaluate

--- test_models.py
+++ test_models.py
@@ -1,50 +1,68 @@
 import os
 import warnings
 import numpy as np
+from skimage.feature import match_template
+from skimage.measure import regionprops
 import skimage.io as ski_io
 from keras.layers import Input
 import keras.backend as K
 from .envs import PJ, this_dir
 from .models import GLCICBuilder
-from .helpers import MaskGenerator
+from .helpers import FixMaskGenerator
+
+
+temp_dir = 'temp'
+os.makedirs(PJ(this_dir, temp_dir), exist_ok=True)
 
 
 if __name__ == '__main__':
     input_image = ski_io.imread(PJ(this_dir, 'data/food.jpg')).astype(np.float) / 255.0
-    input_masks, bboxes = list(MaskGenerator().flow(total_size=1))[0]
+    input_mask, bbox = FixMaskGenerator()._draw(x=120, y=128, w=30, h=50)
     color_prior = np.asarray([10, 255, 10], dtype=np.float) / 255.0
 
-    temp_dir = 'temp'
-    os.makedirs(PJ(this_dir, temp_dir), exist_ok=True)
+    # input_tensor = Input(shape=(256, 256, 3), name='raw_image')
+    # mask_tensor = Input(shape=(256, 256, 1), name='mask')
+    # bbox_tensor = Input(shape=(4,), dtype='int32', name='bounding_box')
 
-    input_tensor = Input(shape=(256, 256, 3), name='raw_image')
-    mask_tensor = Input(shape=(256, 256, 1), name='mask')
-    bbox_tensor = Input(shape=(4,), dtype='int32', name='bounding_box')
+    # glcic = GLCICBuilder(activation='relu', loss=['mse', 'binary_crossentropy'])
+    # glcic_net = glcic(input_tensor, mask_tensor, bbox_tensor,
+    #                   color_prior=color_prior, do_compile=True)
 
-    glcic = GLCICBuilder(activation='relu', loss=['mse', 'binary_crossentropy'])
-    glcic_net = glcic(input_tensor, mask_tensor, bbox_tensor,
-                      color_prior=color_prior, do_compile=True)
+    # # Suppress trainable weights and collected trainable inconsistency warning
+    # with warnings.catch_warnings():
+    #     warnings.simplefilter('ignore', category=UserWarning)
+    #     glcic.nested_summary(glcic_net)
 
-    # Suppress trainable weights and collected trainable inconsistency warning
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore', category=UserWarning)
-        glcic.nested_summary(glcic_net)
+    # completion_net = glcic.glcic_completion
+    # filled_image = K.get_value(
+    #     glcic.completion_builder.preprocessing(
+    #         [input_image, input_masks[0, ...]]
+    #     )
+    # )
 
-    completion_net = glcic.glcic_completion
-    filled_image = K.get_value(
-        glcic.completion_builder.preprocessing(
-            [input_image, input_masks[0, ...]]
-        )
-    )
+    # completed_images = completion_net.predict([np.asarray([input_image]), input_masks])
 
-    completed_images = completion_net.predict([np.asarray([input_image]), input_masks])
+    minr, minc, maxr, maxc = regionprops(input_mask.astype(int))[0].bbox
+    template = input_image[minr:maxr, minc:maxc]
+    attention_map = match_template(input_image, template, pad_input=True).sum(axis=2) * 0.33
+    attention_map[attention_map < 0] = 0
+    attention_map = (attention_map - attention_map.min()) / (attention_map.max() - attention_map.min())
+    attention_map[minr:maxr, minc:maxc] = 0
+    attention_map[:bbox[1], :] = 0
+    attention_map[:, :bbox[0]] = 0
 
-    cropped_image = K.get_value(
-        glcic.discriminator_builder.cropping(filled_image, bboxes[0, ...])
-    )
+    attention_map[bbox[1]+bbox[3]:, :] = 0
+    attention_map[:, bbox[0]+bbox[2]:] = 0
+
+    print(attention_map.shape, attention_map.max(), attention_map.min(), attention_map.dtype)
+
+    # cropped_image = K.get_value(
+    #     glcic.discriminator_builder.cropping(filled_image, bboxes[0, ...])
+    # )
 
     ski_io.imsave(PJ(this_dir, temp_dir, '0_reference.jpg'), input_image, quality=100)
-    ski_io.imsave(PJ(this_dir, temp_dir, '1_mask.jpg'), input_masks[0, :, :, 0], quality=100)
-    ski_io.imsave(PJ(this_dir, temp_dir, '2_preprocessed.jpg'), filled_image, quality=100)
-    ski_io.imsave(PJ(this_dir, temp_dir, '3_completed.jpg'), completed_images[0, ...], quality=100)
-    ski_io.imsave(PJ(this_dir, temp_dir, '4_cropped.jpg'), cropped_image, quality=100)
+    ski_io.imsave(PJ(this_dir, temp_dir, '1_mask.jpg'), input_mask[..., 0], quality=100)
+    # ski_io.imsave(PJ(this_dir, temp_dir, '2_preprocessed.jpg'), filled_image, quality=100)
+    # ski_io.imsave(PJ(this_dir, temp_dir, '3_completed.jpg'), completed_images[0, ...], quality=100)
+    ski_io.imsave(PJ(this_dir, temp_dir, '4_attention.jpg'), attention_map, quality=100)
+    # ski_io.imsave(PJ(this_dir, temp_dir, '5_cropped.jpg'), cropped_image, quality=100)
--- training.py
+++ training.py
@@ -7,7 +7,7 @@ from keras.layers import Input
 from keras.preprocessing.image import ImageDataGenerator
 import keras.backend as K
 from .models import GLCICBuilder
-from .helpers import RandomMaskGenerator
+from .helpers import RandomMaskGenerator, FixMaskGenerator
 from .envs import (
     PJ, data_dir, evaluate_dir, ckpt_dir,
     generator_loss, discriminator_loss,
@@ -18,7 +18,22 @@ os.makedirs(evaluate_dir, exist_ok=True)
 os.makedirs(ckpt_dir, exist_ok=True)
 
 
-def training(x_train, x_test=None,
+def evaluate(x_test, completion_net, w=32, h=32):
+    maskgan = FixMaskGenerator()
+    steps = [0, 32, 64, 96, 128]
+    union_results = x_test[0, ...].copy()
+    for y in steps:
+        for x in steps:
+            p = x + ((128 - w) >> 1)
+            q = y + ((128 - h) >> 1)
+            mask, box = np.asarray([maskgan._draw(x=x, y=y, w=w, h=h)])
+            eval_images = completion_net.predict([x_test, mask])
+            union_results[q:q+h, p:p+w, :] = eval_images[0, q:q+h, p:p+w, :]
+
+    return union_results
+
+
+def training(x_train, x_test=None, init_iters=1,
              eval_iters=50, ckpt_iters=100, max_iters=-1,
              tc_prior=0.3, td_prior=0.6,
              pretrained_generator=PJ(ckpt_dir, 'generator.h5'),
@@ -47,9 +62,9 @@ def training(x_train, x_test=None,
     local_discriminator = glcic.discriminator_builder.local_net
     global_discriminator = glcic.discriminator_builder.global_net
 
-    if x_test is not None:
-        eval_images = K.get_value(glcic.completion_builder.preprocessing(x_test))
-        ski_io.imsave(PJ(evaluate_dir, f'eval_00000.jpg'), eval_images[0, ...], quality=100)
+    # if x_test is not None:
+    #     eval_images = K.get_value(glcic.completion_builder.preprocessing(x_test))
+    #     ski_io.imsave(PJ(evaluate_dir, f'eval_00000.jpg'), eval_images[0, ...], quality=100)
 
     batch_size = x_train.shape[0]
     datagan = ImageDataGenerator(rotation_range=20, horizontal_flip=True,
@@ -62,7 +77,7 @@ def training(x_train, x_test=None,
     with warnings.catch_warnings():
         warnings.simplefilter('ignore', category=UserWarning)
         for i, (images, (masks, bboxes)) in enumerate(zip(datagan.flow(x_train, batch_size=batch_size),
-                                                          maskgan.flow(batch_size=batch_size)), 1):
+                                                          maskgan.flow(batch_size=batch_size)), init_iters):
             completed_images = completion_net.predict([images, masks])
             real = np.ones((batch_size, 1))
             fake = np.zeros((batch_size, 1))
@@ -88,8 +103,10 @@ def training(x_train, x_test=None,
             print(f'Iter: {i:05}\tLosses: generator: {g_loss:.3E}, discriminator: {d_loss:.3E}', flush=True)
 
             if i % eval_iters == 0 and (x_test is not None):
-                eval_images = completion_net.predict(x_test)
-                ski_io.imsave(PJ(evaluate_dir, f'eval_{i:05}.jpg'), eval_images[0, ...], quality=100)
+                # eval_images = completion_net.predict(x_test)
+                eval_image = evaluate(x_test, completion_net)
+
+                ski_io.imsave(PJ(evaluate_dir, f'eval_{i:05}.jpg'), eval_image, quality=100)
             if i % ckpt_iters == 0:
                 completion_net.save(PJ(ckpt_dir, 'generator.h5'))
                 local_discriminator.save(PJ(ckpt_dir, 'local_discriminator.h5'))
@@ -103,5 +120,5 @@ if __name__ == '__main__':
     eval_mask = list(RandomMaskGenerator().flow(total_size=1))[0][0]
     input_image = ski_io.imread(PJ(data_dir, 'food.jpg')).astype(np.float) / 255.0
     x_train = np.asarray([input_image])
-    x_test = [x_train, eval_mask]
-    training(x_train, x_test=x_test)
+    # x_test = [x_train, eval_mask]
+    training(x_train, x_test=x_train)
